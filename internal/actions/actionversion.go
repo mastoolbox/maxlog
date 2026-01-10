dd2c1d0004b548feef2a5f075b5dc80e4b4c96ee
ageeye <5332429+ageeye@users.noreply.github.com>
2026-01-10 23:01:18 +0000

    Fix LF

--- internal/actions/actionversion.go
+++ internal/actions/actionversion.go
@@ -1,32 +1,34 @@
 package actions
 
 import (
-    "fmt"
+	"fmt"
 )
 
 // ActionVersion creates and initializes an Action for displaying version information.
 //
 // Returns:
-//   *Action - A pointer to the initialized Action instance.
+//
+//	*Action - A pointer to the initialized Action instance.
 //
 // Behavior:
 //   - Sets the name of the Action to "version".
 //   - Assigns the runVersion function to the Action's runAction field.
 func ActionVersion() *Action {
-    act := &Action{
-        name: "version",
-    }
-    act.runAction = runVersion
-    return act
+	act := &Action{
+		name: "version",
+	}
+	act.runAction = runVersion
+	return act
 }
 
 // runVersion displays the version information of the application.
 //
 // Parameters:
-//   act - A pointer to the Action instance.
+//
+//	act - A pointer to the Action instance.
 //
 // Behavior:
 //   - Prints the current version of the application to the console.
 func runVersion(act *Action) {
-    fmt.Println("maxlog version: 0.0.3")
+	fmt.Println("maxlog version: 0.0.3")
 }
